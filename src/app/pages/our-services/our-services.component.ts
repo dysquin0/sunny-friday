770a9ba01dc708e17cfd585b22c10a5985541409
dysquin0 <quinogladysclaire@gmail.com>
2022-06-27 16:58:02 +0800

    card clicks update

--- src/app/pages/our-services/our-services.component.ts
+++ src/app/pages/our-services/our-services.component.ts
@@ -17,18 +17,57 @@ export class OurServicesComponent implements OnInit {
       title: 'Content Marketing',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+      paragraphs: {
+        p1: 'Useful content should be at the core of your marketing.. ',
+        p2: 'Traditional marketing is becoming less and less effective by the minute; as a forward-thinking marketer, you know there has to be a better way.',
+        p3: 'Instead of pitching your products or services, you are providing truly relevant and useful content to your prospects and customers to help them solve their issues.',
+        p4: 'Our annual research shows the vast majority of marketers are using content marketing. In fact, it is used by many prominent organizations in the world, including P&G, Microsoft, Cisco Systems, and John Deere. It’s also developed and executed by small businesses and one-person shops around the globe. Why? Because it works.',
+        p5: 'Content marketing is good for your bottom line — and your customers.',
+        p6: 'Specifically, there are four key reasons – and benefits – for enterprises to use content marketing:',
+        //other variables for li
+        li1: 'Increased sales',
+        li2: 'Cost savings',
+        li3: 'Better customer who have more loyalty',
+        li4: 'Content as a profit center',
+      },
     },
     {
       image: '../../../../../assets/images/conversion-rate-optimization.png',
       title: 'Conversion Rate Optimization',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+      paragraphs: {
+        p1: 'Useful content should be at the core of your marketing.. ',
+        p2: 'Traditional marketing is becoming less and less effective by the minute; as a forward-thinking marketer, you know there has to be a better way.',
+        p3: 'Instead of pitching your products or services, you are providing truly relevant and useful content to your prospects and customers to help them solve their issues.',
+        p4: 'Our annual research shows the vast majority of marketers are using content marketing. In fact, it is used by many prominent organizations in the world, including P&G, Microsoft, Cisco Systems, and John Deere. It’s also developed and executed by small businesses and one-person shops around the globe. Why? Because it works.',
+        p5: 'Content marketing is good for your bottom line — and your customers.',
+        p6: 'Specifically, there are four key reasons – and benefits – for enterprises to use content marketing:',
+        //other variables for li
+        li1: 'Increased sales',
+        li2: 'Cost savings',
+        li3: 'Better customer who have more loyalty',
+        li4: 'Content as a profit center',
+      },
     },
     {
       image: '../../../../../assets/images/email-marketing.png',
       title: 'Email Marketing',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+      paragraphs: {
+        p1: 'Useful content should be at the core of your marketing.. ',
+        p2: 'Traditional marketing is becoming less and less effective by the minute; as a forward-thinking marketer, you know there has to be a better way.',
+        p3: 'Instead of pitching your products or services, you are providing truly relevant and useful content to your prospects and customers to help them solve their issues.',
+        p4: 'Our annual research shows the vast majority of marketers are using content marketing. In fact, it is used by many prominent organizations in the world, including P&G, Microsoft, Cisco Systems, and John Deere. It’s also developed and executed by small businesses and one-person shops around the globe. Why? Because it works.',
+        p5: 'Content marketing is good for your bottom line — and your customers.',
+        p6: 'Specifically, there are four key reasons – and benefits – for enterprises to use content marketing:',
+        //other variables for li
+        li1: 'Increased sales',
+        li2: 'Cost savings',
+        li3: 'Better customer who have more loyalty',
+        li4: 'Content as a profit center',
+      },
     },
   ];
   rowTwo = [
@@ -37,18 +76,57 @@ export class OurServicesComponent implements OnInit {
       title: 'Pay-per-click Marketing',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+      paragraphs: {
+        p1: 'Useful content should be at the core of your marketing.. ',
+        p2: 'Traditional marketing is becoming less and less effective by the minute; as a forward-thinking marketer, you know there has to be a better way.',
+        p3: 'Instead of pitching your products or services, you are providing truly relevant and useful content to your prospects and customers to help them solve their issues.',
+        p4: 'Our annual research shows the vast majority of marketers are using content marketing. In fact, it is used by many prominent organizations in the world, including P&G, Microsoft, Cisco Systems, and John Deere. It’s also developed and executed by small businesses and one-person shops around the globe. Why? Because it works.',
+        p5: 'Content marketing is good for your bottom line — and your customers.',
+        p6: 'Specifically, there are four key reasons – and benefits – for enterprises to use content marketing:',
+        //other variables for li
+        li1: 'Increased sales',
+        li2: 'Cost savings',
+        li3: 'Better customer who have more loyalty',
+        li4: 'Content as a profit center',
+      },
     },
     {
       image: '../../../../../assets/images/public-relations.png',
       title: 'Public Relations',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+      paragraphs: {
+        p1: 'Useful content should be at the core of your marketing.. ',
+        p2: 'Traditional marketing is becoming less and less effective by the minute; as a forward-thinking marketer, you know there has to be a better way.',
+        p3: 'Instead of pitching your products or services, you are providing truly relevant and useful content to your prospects and customers to help them solve their issues.',
+        p4: 'Our annual research shows the vast majority of marketers are using content marketing. In fact, it is used by many prominent organizations in the world, including P&G, Microsoft, Cisco Systems, and John Deere. It’s also developed and executed by small businesses and one-person shops around the globe. Why? Because it works.',
+        p5: 'Content marketing is good for your bottom line — and your customers.',
+        p6: 'Specifically, there are four key reasons – and benefits – for enterprises to use content marketing:',
+        //other variables for li
+        li1: 'Increased sales',
+        li2: 'Cost savings',
+        li3: 'Better customer who have more loyalty',
+        li4: 'Content as a profit center',
+      },
     },
     {
       image: '../../../../../assets/images/review-management.png',
       title: 'Review Management',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+      paragraphs: {
+        p1: 'Useful content should be at the core of your marketing.. ',
+        p2: 'Traditional marketing is becoming less and less effective by the minute; as a forward-thinking marketer, you know there has to be a better way.',
+        p3: 'Instead of pitching your products or services, you are providing truly relevant and useful content to your prospects and customers to help them solve their issues.',
+        p4: 'Our annual research shows the vast majority of marketers are using content marketing. In fact, it is used by many prominent organizations in the world, including P&G, Microsoft, Cisco Systems, and John Deere. It’s also developed and executed by small businesses and one-person shops around the globe. Why? Because it works.',
+        p5: 'Content marketing is good for your bottom line — and your customers.',
+        p6: 'Specifically, there are four key reasons – and benefits – for enterprises to use content marketing:',
+        //other variables for li
+        li1: 'Increased sales',
+        li2: 'Cost savings',
+        li3: 'Better customer who have more loyalty',
+        li4: 'Content as a profit center',
+      },
     },
   ];
 
@@ -58,18 +136,57 @@ export class OurServicesComponent implements OnInit {
       title: 'Search engine optimization (SEO)',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+      paragraphs: {
+        p1: 'Useful content should be at the core of your marketing.. ',
+        p2: 'Traditional marketing is becoming less and less effective by the minute; as a forward-thinking marketer, you know there has to be a better way.',
+        p3: 'Instead of pitching your products or services, you are providing truly relevant and useful content to your prospects and customers to help them solve their issues.',
+        p4: 'Our annual research shows the vast majority of marketers are using content marketing. In fact, it is used by many prominent organizations in the world, including P&G, Microsoft, Cisco Systems, and John Deere. It’s also developed and executed by small businesses and one-person shops around the globe. Why? Because it works.',
+        p5: 'Content marketing is good for your bottom line — and your customers.',
+        p6: 'Specifically, there are four key reasons – and benefits – for enterprises to use content marketing:',
+        //other variables for li
+        li1: 'Increased sales',
+        li2: 'Cost savings',
+        li3: 'Better customer who have more loyalty',
+        li4: 'Content as a profit center',
+      },
     },
     {
       image: '../../../../../assets/images/social-media-advertising.png',
       title: 'Social Media Advertising',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+      paragraphs: {
+        p1: 'Useful content should be at the core of your marketing.. ',
+        p2: 'Traditional marketing is becoming less and less effective by the minute; as a forward-thinking marketer, you know there has to be a better way.',
+        p3: 'Instead of pitching your products or services, you are providing truly relevant and useful content to your prospects and customers to help them solve their issues.',
+        p4: 'Our annual research shows the vast majority of marketers are using content marketing. In fact, it is used by many prominent organizations in the world, including P&G, Microsoft, Cisco Systems, and John Deere. It’s also developed and executed by small businesses and one-person shops around the globe. Why? Because it works.',
+        p5: 'Content marketing is good for your bottom line — and your customers.',
+        p6: 'Specifically, there are four key reasons – and benefits – for enterprises to use content marketing:',
+        //other variables for li
+        li1: 'Increased sales',
+        li2: 'Cost savings',
+        li3: 'Better customer who have more loyalty',
+        li4: 'Content as a profit center',
+      },
     },
     {
       image: '../../../../../assets/images/social-media-management.png',
       title: 'Social Media Management',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+      paragraphs: {
+        p1: 'Useful content should be at the core of your marketing.. ',
+        p2: 'Traditional marketing is becoming less and less effective by the minute; as a forward-thinking marketer, you know there has to be a better way.',
+        p3: 'Instead of pitching your products or services, you are providing truly relevant and useful content to your prospects and customers to help them solve their issues.',
+        p4: 'Our annual research shows the vast majority of marketers are using content marketing. In fact, it is used by many prominent organizations in the world, including P&G, Microsoft, Cisco Systems, and John Deere. It’s also developed and executed by small businesses and one-person shops around the globe. Why? Because it works.',
+        p5: 'Content marketing is good for your bottom line — and your customers.',
+        p6: 'Specifically, there are four key reasons – and benefits – for enterprises to use content marketing:',
+        //other variables for li
+        li1: 'Increased sales',
+        li2: 'Cost savings',
+        li3: 'Better customer who have more loyalty',
+        li4: 'Content as a profit center',
+      },
     },
   ];
 }
