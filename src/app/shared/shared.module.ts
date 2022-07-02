3c10e2e3f11850420cae731897bbc2ca6b0c5189
dysquin0 <quinogladysclaire@gmail.com>
2022-07-02 20:31:31 +0800

    updated shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  L10N_CONFIG,
-  L10nConfig,
-  L10N_LOCALE,
-  L10nLocale,
-  L10nTranslationService,
-} from 'angular-l10n';
-import { L10nSchema } from 'angular-l10n/lib/models/types';
 
 const sharedModules: any[] = [
   CommonModule,
